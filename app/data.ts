dff771b2044ac1c8b12defaa21d88bac4613c10d
Yaswanth Koneri <yashwanth@give.do>
2024-05-01 18:44:23 +0530

    task api added

--- app/data.ts
+++ app/data.ts
@@ -24,7 +24,7 @@ export type TaskRecord = TaskMutation & {
 ////////////////////////////////////////////////////////////////////////////////
 // This is just a fake DB table. In a real app you'd be talking to a real db or
 // fetching from an existing API.
-const fakeTasks = {
+const TaskApi = {
   records: {} as Record<string, TaskRecord>,
 
   async getAll(token: string): Promise<TaskRecord[]> {
@@ -47,17 +47,7 @@ const fakeTasks = {
     return data;
   },
 
-  async get(id: string): Promise<TaskRecord | null> {
-    return fakeTasks.records[id] || null;
-  },
-
-  async set(id: string, values: TaskMutation): Promise<TaskRecord> {
-    const contact = await fakeTasks.get(id);
-    invariant(contact, `No contact found for ${id}`);
-    const updatedContact = { ...contact, ...values };
-    fakeTasks.records[id] = updatedContact;
-    return updatedContact;
-  }
+ 
 };
 
 export const getJWTToken = async (request: any) => {
@@ -84,10 +74,10 @@ export async function getContacts(query?: string | null, token?: string) {
   try {
     let contacts
     if (query) {
-      contacts = await fakeTasks.getAllSearch(query, token);
+      contacts = await TaskApi.getAllSearch(query, token);
       return contacts || []
     }
-    contacts = await fakeTasks.getAll(token) || [];
+    contacts = await TaskApi.getAll(token) || [];
 
     // console.log("contacts", contacts)
     if (contacts.code === 'token_not_valid') {
@@ -113,12 +103,7 @@ export async function getContact(id: string, token: string) {
 }
 
 export async function updateContact(id: string, updates: TaskMutation, token: string) {
-  // const contact = await fakeTasks.get(id);
-  // if (!contact) {
-  //   throw new Error(`No contact found for ${id}`);
-  // }
-  // await fakeTasks.set(id, { ...contact, ...updates });
-  // return contact;
+ 
 
 
   const response = await fetch(`${BASE_URL}/tasks/${id}`, {
